bec9c60219596e3c17a1c6da511bebbdc2c81879
Moisés Gomes <moisesgomes@MBBLUE-000035.local>
2025-02-22 14:32:40 -0300

    Corrigido problema de repositório aninhado e adicionado corretamente

---
+++ bolt.new/app/components/header/Header.tsx
@@ -0,0 +1,49 @@
+import { useStore } from '@nanostores/react';
+import { ClientOnly } from 'remix-utils/client-only';
+import { chatStore } from '~/lib/stores/chat';
+import { classNames } from '~/utils/classNames';
+import { HeaderActionButtons } from './HeaderActionButtons.client';
+import { ChatDescription } from '~/lib/persistence/ChatDescription.client';
+import logo from "../../../icons/logo-text.svg"
+
+export function Header() {
+  const chat = useStore(chatStore);
+
+  return (
+    <header
+      className={classNames(
+        'flex items-center bg-bolt-elements-background-depth-1 p-5 border-b h-[var(--header-height)]',
+        {
+          'border-transparent': !chat.started,
+          'border-bolt-elements-borderColor': chat.started,
+        },
+      )}
+    >
+      <div className="flex items-center gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
+        <div className="i-ph:sidebar-simple-duotone text-xl" />
+        <a href="/" className="text-2xl font-semibold text-accent flex items-center h-8">
+  <img 
+    src={logo}
+    alt="Logo" 
+    className="h-20 w-20 object-contain"
+    style={{
+      filter: 'brightness(0) invert(1)' // This will make the logo white
+    }}
+  />
+</a>
+      </div>
+      <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
+        <ClientOnly>{() => <ChatDescription />}</ClientOnly>
+      </span>
+      {chat.started && (
+        <ClientOnly>
+          {() => (
+            <div className="mr-1">
+              <HeaderActionButtons />
+            </div>
+          )}
+        </ClientOnly>
+      )}
+    </header>
+  );
+}
